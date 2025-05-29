48193bc7ab0745a6561d0709c18bf2a9932b3be3
joe342wise <jnyarko342@gmail.com>
2025-05-29 01:13:37 +0000

    Add pino logging middleware and update dependencies in package.json

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,30 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
+import { notFound, onError } from "stoker/middlewares";
 
-const app = new OpenAPIHono();
+import { pinoLogger } from "./Middlewares/pino-logger.js";
+import { PinoLogger } from "hono-pino";
+
+
+type AppBindings = {
+    Variables: {
+        logger: PinoLogger;
+    }
+};
+
+const app = new OpenAPIHono<AppBindings>();
+app.use(pinoLogger());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+app.get("/error", (c) => {
+  c.status(500);
+  c.var.logger.info("wow log hear");
+  throw new Error("This is a test error");
+});
+
+app.notFound(notFound);
+app.onError(onError);
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { serve } from "@hono/node-server";
 
 import app from "./app.js";
 
-
 serve({
   fetch: app.fetch,
   port: 3000,
